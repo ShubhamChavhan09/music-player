a5d461acdb13798f83bfa22316265566dda3dd64
Shubham <shubh.chavhan00@gmail.com>
2021-12-07 16:24:44 +0530

    Updated the Container path

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Container from "./components/Container";
+import Container from "./components/container";
 import GlobalStyle from "./globalStyles";
 import styled from "styled-components";
 
