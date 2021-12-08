4aa2176acb7b4a9b46251dc07dee7fb7dc9758d3
Shubham <shubh.chavhan00@gmail.com>
2021-12-08 22:29:36 +0530

    Component with album details

--- src/components/info-container/index.js
+++ src/components/info-container/index.js
@@ -1,9 +1,36 @@
+import { RiHeartFill, RiPlayListAddFill, RiShareFill } from "react-icons/ri";
 import styled from "styled-components";
+import Purple from "../../data/Purple.jpg";
 
 const InfoContainer = () => {
   return (
     <Info>
-      <h1>Music Player</h1>
+      <Data>
+        <Span>
+          <img src={Purple} alt="Purple Haze cover" />
+        </Span>
+      </Data>
+      <Data>
+        <div>
+          <p>Now Playing</p>
+        </div>
+        <div>
+          <h3>Purple Haze</h3>
+          <Name>Jimi Hendrix</Name>
+          <p>Woodstock </p>
+        </div>
+        <Links>
+          <Circle>
+            <RiHeartFill />
+          </Circle>
+          <Circle>
+            <RiPlayListAddFill />
+          </Circle>
+          <Circle>
+            <RiShareFill />
+          </Circle>
+        </Links>
+      </Data>
     </Info>
   );
 };
@@ -15,6 +42,82 @@ const Info = styled.div`
   height: 257px;
   width: 436px;
   z-index: 1;
+  border: 1px solid #ffffff;
   border-radius: 33px;
-  padding: 27px;
+  // box-shadow: 20px 20px 60px #c0bcd9, -20px -20px 60px #fffeff;
+  box-shadow: 22px 16px 44px rgba(54, 48, 116, 0.3);
+  display: flex;
+`;
+
+const Data = styled.div`
+  flex: 1;
+  padding: 27px 0 27px 27px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  h3,
+  p {
+    margin: 0;
+  }
+
+  h3 {
+    color: #86748d;
+    font-size: 17px;
+  }
+
+  img {
+    width: 100%;
+    border-radius: 16px;
+    box-shadow: inset 5px 5px 8px rgba(255, 255, 255, 0.2),
+      2.5px 2.5px 5px rgba(37, 5, 57, 0.51);
+  }
+`;
+
+const Span = styled.span`
+  height: 200px;
+  width: 200px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  border-radius: 22px;
+  overflow: hidden;
+  padding: 6px;
+  // box-shadow: inset 5px 5px 5px rgba(0, 0, 0, 0.05),
+  //   inset -5px -5px 11px #ffffff, inset 5px 5px 5px rgba(0, 0, 0, 0.05),
+  //   inset -5px -5px 11px #ffffff;
+
+  box-shadow: inset 5px 5px 5px rgba(0, 0, 0, 0.05),
+    inset -5px -5px 11px #ffffff, -5px -5px 8px #ffffff,
+    5px 5px 5px rgba(0, 0, 0, 0.05);
+`;
+
+const Name = styled.p`
+  font-size: 14px;
+`;
+
+const Circle = styled.span`
+  height: 25px;
+  width: 25px;
+  margin-right: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #f8f8f8;
+  border-radius: 100%;
+  text-align: center;
+  font-size: 14px;
+  box-shadow: -5px -5px 8px #ffffff, 5px 5px 5px rgba(0, 0, 0, 0.05),
+    inset 5px 5px 5px rgba(0, 0, 0, 0.05), inset -5px -5px 11px #ffffff;
+
+  &:active {
+    box-shadow: inset -5px -5px 8px #ffffff,
+      inset 5px 5px 5px rgba(0, 0, 0, 0.05);
+  }
+`;
+
+const Links = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 `;
