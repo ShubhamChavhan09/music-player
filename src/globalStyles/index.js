cbcf198ee16299cd1609689c8b226d6b513a9bb9
Shubham <shubh.chavhan00@gmail.com>
2021-12-09 19:13:33 +0530

    Added default root widt for the seek-bar

--- src/globalStyles/index.js
+++ src/globalStyles/index.js
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+
+:root{
+    --seek-width: 0;
+    --primary: linear-gradient(327.56deg, #5d24d6 19.23%, #7e74ed 81.76%);
+   
+}
 *{
     box-sizing: border-box;
 }
