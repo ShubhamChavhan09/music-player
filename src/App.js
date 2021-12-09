afea2db9297f40360e6bbb3447de835338434817
Shubham <shubh.chavhan00@gmail.com>
2021-12-09 15:16:14 +0530

    Added flex properties and removed fixed width

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ export default App;
 
 const Wrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   height: 100vh;
-  width: 100vw;
+  margin-bottom: 50px;
 `;
